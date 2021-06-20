2c88bf24ba4ed7ae844e6a3a44638d6eedd70e73
ultramarine11 <36325179+ultramarine11@users.noreply.github.com>
2021-06-20 22:18:16 +0300

    added Liquid Matter Collector logic

--- src/main/java/ru/wirelesstools/Reference.java
+++ src/main/java/ru/wirelesstools/Reference.java
@@ -3,9 +3,7 @@ package ru.wirelesstools;
 public class Reference {
 	
 	public static final String NAME = "wirelessindustry";
-	public static final String TEXTURES_BLOCKS = "wirelessindustry:textures/blocks/";
-	public static final String TEXTURES_ITEMS = "wirelessindustry:textures/items/";
-	
+
 	public static final String PathTex = "wirelessindustry:";
 
 }
--- src/main/java/ru/wirelesstools/tiles/TileLiquidMatterCollector.java
+++ src/main/java/ru/wirelesstools/tiles/TileLiquidMatterCollector.java
@@ -1,47 +1,100 @@
 package ru.wirelesstools.tiles;
 
+import ic2.core.block.TileEntityInventory;
+import ic2.core.block.TileEntityLiquidTankElectricMachine;
+import ic2.core.block.invslot.InvSlot;
+import ic2.core.block.invslot.InvSlotConsumableLiquid;
+import ic2.core.block.invslot.InvSlotConsumableLiquidByList;
+import ic2.core.block.invslot.InvSlotOutput;
+import ic2.core.init.BlocksItems;
+import ic2.core.init.InternalName;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
-import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.FluidTankInfo;
-import net.minecraftforge.fluids.IFluidHandler;
+import net.minecraftforge.fluids.*;
 
-public class TileLiquidMatterCollector extends TileEntity implements IFluidHandler {
+import java.util.ArrayList;
 
-    /*public final InvSlotOutput outputSlot;
+public class TileLiquidMatterCollector extends TileEntityInventory implements IFluidHandler {
+
+    public final InvSlotOutput outputSlot;
     public final InvSlotConsumableLiquid containerslot;
     protected final FluidTank fluidTank;
-    public final String mattercollectorname;*/
+    public final String mattercollectorname;
 
+    public TileLiquidMatterCollector(String collectorname) {
+        this.fluidTank = new FluidTank(1000000);
+        this.mattercollectorname = collectorname;
+        this.outputSlot = new InvSlotOutput(this, "output", 1, 1);
+        this.containerslot = new InvSlotConsumableLiquidByList(this, "containerslot", 2, InvSlot.Access.I, 1, InvSlot.InvSide.TOP, InvSlotConsumableLiquid.OpType.Fill, new Fluid[]{BlocksItems.getFluid(InternalName.fluidUuMatter)});
+    }
+
+    @Override
+    protected void updateEntityServer() {
+        super.updateEntityServer();
+        if(!this.worldObj.getChunkFromBlockCoords(this.xCoord, this.zCoord).chunkTileEntityMap.isEmpty()) {
+            for (TileEntity tile : new ArrayList<TileEntity>(this.getWorldObj()
+                    .getChunkFromBlockCoords(this.xCoord, this.zCoord).chunkTileEntityMap.values())) {
+                if(tile instanceof TileEntityLiquidTankElectricMachine) {
+                    this.tryFillCollector((TileEntityLiquidTankElectricMachine)tile);
+                }
+            }
+        }
+    }
+
+    public FluidTank getFluidTank() {
+        return this.fluidTank;
+    }
+
+    private void tryFillCollector(TileEntityLiquidTankElectricMachine tiletank) {
+        if(tiletank.getFluidStackfromTank().getFluid() != null
+                && tiletank.getFluidStackfromTank().getFluid() == BlocksItems.getFluid(InternalName.fluidUuMatter)) {
+            if(tiletank.getTankAmount() > 0) {
+                FluidStack drainedsim = tiletank.getFluidTank().drain(tiletank.getTankAmount(), false);
+                if(drainedsim != null) {
+                    int fillsim = this.getFluidTank().fill(drainedsim, false);
+                    int realtransferred = Math.min(fillsim, drainedsim.amount);
+                    this.getFluidTank().fill(tiletank.getFluidTank().drain(realtransferred, true), true);
+                }
+            }
+        }
+    }
 
     @Override
     public int fill(ForgeDirection from, FluidStack resource, boolean doFill) {
-        return 0;
+        return !this.canFill(from, resource.getFluid()) ? 0 : this.getFluidTank().fill(resource, doFill);
     }
 
     @Override
     public FluidStack drain(ForgeDirection from, FluidStack resource, boolean doDrain) {
-        return null;
+        if (resource != null && resource.isFluidEqual(this.getFluidTank().getFluid())) {
+            return !this.canDrain(from, resource.getFluid()) ? null : this.getFluidTank().drain(resource.amount, doDrain);
+        } else {
+            return null;
+        }
     }
 
     @Override
     public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain) {
-        return null;
+        return !this.canDrain(from, null) ? null : this.getFluidTank().drain(maxDrain, doDrain);
     }
 
     @Override
     public boolean canFill(ForgeDirection from, Fluid fluid) {
-        return false;
+        return fluid == BlocksItems.getFluid(InternalName.fluidUuMatter);
     }
 
     @Override
     public boolean canDrain(ForgeDirection from, Fluid fluid) {
-        return false;
+        return true;
     }
 
     @Override
     public FluidTankInfo[] getTankInfo(ForgeDirection from) {
-        return new FluidTankInfo[0];
+        return new FluidTankInfo[]{this.getFluidTank().getInfo()};
+    }
+
+    @Override
+    public String getInventoryName() {
+        return null;
     }
 }
